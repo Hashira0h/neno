6d3d3e9b13a32cb3ad7c6077819c3c5469b400c7
Hashira0h <139925219+Hashira0h@users.noreply.github.com>
2023-07-25 22:02:45 -0700

    Update main-tagall.js

--- plugins/main-tagall.js
+++ plugins/main-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`⧉ جروب : \n*${groupMetadata.subject}*\n⧉ عدد الاعضاء : *${participants.length}*${text ? `\n⧉ الرساله : ${text}\n` : ''}\n【 MENTIONS 】\n` + users.map(v => '■ @' + v.replace(/@.+/, '')).join`\n` + '\n【 Thank you 】', null, {
+    m.reply(`⧉ جروب : \n*${groupMetadata.subject}*\n⧉ عدد الاعضاء : *${participants.length}*${text ? `\n⧉ الرساله : ${text}\n` : ''}\n*❆──━━┈ ⊰🌩⊱ ┈━━──❆*\n【 المنشن 】\n` + users.map(v => '🌩 @' + v.replace(/@.+/, '')).join`\n` + '\n*❆──━━┈ ⊰🌩⊱ ┈━━──❆*\n【 نينو 】', null, {
         mentions: users
     })
 }
